3b3f397461cc674545602eb016816a00ce6966c9
duro92 <pendisu647@gmail.com>
2025-09-11 11:39:05 +0700

    add github actions build workflow

---
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
@@ -0,0 +1,17 @@
+package com.dutamovie
+
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.JWPlayer
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DutaMoviePlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DutaMovie())
+        registerExtractorAPI(Helvid())
+        registerExtractorAPI(JWPlayer())
+        registerExtractorAPI(Embedpyrox())
+        registerExtractorAPI(p2pplay())
+    }
+}
---
+++ Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
@@ -0,0 +1,19 @@
+package com.dutamovie
+
+import com.lagradost.cloudstream3.extractors.JWPlayer
+
+
+class Embedpyrox : JWPlayer() {
+    override var name = "Embedpyrox"
+    override var mainUrl = "https://embedpyrox.xyz"
+}
+
+class Helvid : JWPlayer() {
+    override var name = "Helvid"
+    override var mainUrl = "https://helvid.net"
+}
+
+class p2pplay : JWPlayer() {
+    override var name = "p2pplay"
+    override var mainUrl = "https://pm21.p2pplay.pro"
+}
