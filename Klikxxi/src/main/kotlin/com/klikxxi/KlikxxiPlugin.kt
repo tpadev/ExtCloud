04f2b7e7660dff111508b2e7a70071609d11aa3d
duro92 <pendisu647@gmail.com>
2025-09-16 18:13:36 +0700

    tambah provider lagi

---
+++ Klikxxi/src/main/kotlin/com/klikxxi/KlikxxiPlugin.kt
@@ -0,0 +1,12 @@
+package com.klikxxi
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class KlikxxiPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Klikxxi())
+    }
+}
