7416d8e9e94b915ffc3c959810c1e76bc40b0758
duro92 <pendisu647@gmail.com>
2025-09-16 11:31:32 +0700

    gak tau lagi

--- Dutamovie/src/main/kotlin/com/dutamovie/extractors/Helvid.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/extractors/Helvid.kt
@@ -5,4 +5,5 @@ import com.lagradost.cloudstream3.extractors.JWPlayer
 class Helvid : JWPlayer() {
     override var name = "Helvid"
     override var mainUrl = "https://helvid.net"
+    override val requiresReferer = true
 }
