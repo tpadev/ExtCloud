122792f5aebb17fcf7650d66bfc4e04b56a0ff3b
duro92 <pendisu647@gmail.com>
2025-09-16 14:22:58 +0700

    try

--- Ngefilm/src/main/kotlin/com/ngefilm/NgefilmProvider.kt
+++ Ngefilm/src/main/kotlin/com/ngefilm/NgefilmProvider.kt
@@ -3,11 +3,13 @@ package com.ngefilm
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.ngefilm.extractors.Chillx
+import com.ngefilm.extractors.KrakenFilesEmbed
 
 @CloudstreamPlugin
 class NgefilmProvider : BasePlugin() {
     override fun load() {
         registerMainAPI(Ngefilm())
         registerExtractorAPI(Chillx())
+        registerExtractorAPI(KrakenFilesEmbed())
     }
 }
