2820de005b830fc32b40a59fd6c6f1bce97a0e8f
duro92 <pendisu647@gmail.com>
2025-09-18 09:54:12 +0700

    Update

--- Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
@@ -2,17 +2,18 @@ package com.dutamovie
 
 import com.lagradost.cloudstream3.extractors.JWPlayer
 
-class EmbedPyrox : JWPlayer() {
+
+class embedpyrox : JWPlayer() {
     override var name = "embedpyrox"
     override var mainUrl = "https://embedpyrox.xyz"
 }
 
-class Helvid : JWPlayer() {
+class helvid : JWPlayer() {
     override var name = "helvid"
     override var mainUrl = "https://helvid.net"
 }
 
-class P2PPlay : JWPlayer() {
+class p2pplay : JWPlayer() {
     override var name = "p2pplay"
     override var mainUrl = "https://pm21.p2pplay.pro"
 }
