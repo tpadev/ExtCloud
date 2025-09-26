ed25435d39dd0114bacff686981a8d5fbf3ed65c
duro92 <pendisu647@gmail.com>
2025-09-26 13:13:58 +0700

    Fix

--- Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
@@ -10,6 +10,6 @@ class DutaMoviePlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(DutaMovie())
         registerExtractorAPI(Dingtezuni())
-        registerExtractorAPI(Embedprox())
+        registerExtractorAPI(EmbedProx())
     }
 }
