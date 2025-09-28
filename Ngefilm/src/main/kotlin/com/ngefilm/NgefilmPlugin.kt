d60bfc683098a98f18fe30f7c74e45b74da57c52
duro92 <pendisu647@gmail.com>
2025-09-28 12:53:35 +0700

    Add Ngefilm

---
+++ Ngefilm/src/main/kotlin/com/ngefilm/NgefilmPlugin.kt
@@ -0,0 +1,14 @@
+package com.ngefilm
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class NgefilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Ngefilm())
+        registerExtractorAPI(Dingtezuni())
+        
+    }
+}
