fc3a10432d1ea0cf68b1cdceadbc07c79eb7c53c
duro92 <pendisu647@gmail.com>
2025-10-01 11:01:46 +0700

    Fix

--- Nomat/src/main/kotlin/com/nomat/Nomat.kt
+++ Nomat/src/main/kotlin/com/nomat/Nomat.kt
@@ -18,7 +18,7 @@ class Nomat : MainAPI() {
 
     override var mainUrl = "https://nomat.site"
     private var directUrl: String? = null
-    override var name = "DutaMovie"
+    override var name = "Nomat"
     override val hasMainPage = true
     override var lang = "id"
     override val supportedTypes =
