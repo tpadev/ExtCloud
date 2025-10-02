cd5a751a1570dd384e1e3b93530f9dee9edab668
Pendztagram <114850487+duro92@users.noreply.github.com>
2025-10-02 18:40:53 +0700

    Update Extractors.kt

--- IdlixProvider/src/main/kotlin/com/hexated/Extractors.kt
+++ IdlixProvider/src/main/kotlin/com/hexated/Extractors.kt
@@ -1,18 +1,19 @@
 package com.hexated
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.hexated.IdlixProvider.ResponseSource
+import com.hexated.IdlixProvider.Tracks
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.AppUtils
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.getAndUnpack
 import com.lagradost.cloudstream3.utils.newExtractorLink
 
-open class IdlixPlayer : ExtractorApi() {
-    override val name = "Jeniusplay"
-    override val mainUrl = "https://jeniusplay.com"
+class Jeniusplay : ExtractorApi() {
+    override var name = "Jeniusplay"
+    override var mainUrl = "https://jeniusplay.com"
     override val requiresReferer = true
 
     override suspend fun getUrl(
@@ -21,33 +22,30 @@ open class IdlixPlayer : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val document = app.get(url, referer = "$mainUrl/").document
+        val document = app.get(url, referer = referer).document
         val hash = url.split("/").last().substringAfter("data=")
 
         val m3uLink = app.post(
             url = "$mainUrl/player/index.php?data=$hash&do=getVideo",
             data = mapOf("hash" to hash, "r" to "$referer"),
-            referer = url,
+            referer = referer,
             headers = mapOf("X-Requested-With" to "XMLHttpRequest")
         ).parsed<ResponseSource>().videoSource
 
         callback.invoke(
             newExtractorLink(
-                this.name,
-                this.name,
-                m3uLink,
+                name,
+                name,
+                url = m3uLink,
                 ExtractorLinkType.M3U8
-            ) {
-                this.referer = url
-            }
+            )
         )
-
 
         document.select("script").map { script ->
             if (script.data().contains("eval(function(p,a,c,k,e,d)")) {
                 val subData =
                     getAndUnpack(script.data()).substringAfter("\"tracks\":[").substringBefore("],")
-                tryParseJson<List<Tracks>>("[$subData]")?.map { subtitle ->
+                AppUtils.tryParseJson<List<Tracks>>("[$subData]")?.map { subtitle ->
                     subtitleCallback.invoke(
                         SubtitleFile(
                             getLanguage(subtitle.label ?: ""),
@@ -59,6 +57,7 @@ open class IdlixPlayer : ExtractorApi() {
         }
     }
 
+
     private fun getLanguage(str: String): String {
         return when {
             str.contains("indonesia", true) || str
@@ -66,16 +65,4 @@ open class IdlixPlayer : ExtractorApi() {
             else -> str
         }
     }
-
-    data class ResponseSource(
-        @JsonProperty("hls") val hls: Boolean,
-        @JsonProperty("videoSource") val videoSource: String,
-        @JsonProperty("securedLink") val securedLink: String?,
-    )
-
-    data class Tracks(
-        @JsonProperty("kind") val kind: String?,
-        @JsonProperty("file") val file: String,
-        @JsonProperty("label") val label: String?,
-    )
 }
