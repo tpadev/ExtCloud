7ad7a0a57924a3656a62b2bb93e4f178e5f3cf17
duro92 <pendisu647@gmail.com>
2025-10-09 11:39:00 +0700

    Fix

--- Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
@@ -10,7 +10,6 @@ import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.extractors.StreamWishExtractor
-import com.lagradost.cloudstream3.extractors.Vidguard
 import java.net.URI
 
 class Mivalyo : Dingtezuni() {
@@ -80,9 +79,114 @@ open class Dingtezuni : ExtractorApi() {
 
 }
 
-class Embedpyrox : Vidguardto() {
+open class Embedpyrox : ExtractorApi() {
     override val name = "EmbedPyrox"
     override val mainUrl = "https://embedpyrox.xyz"
+    override val requiresReferer = false
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val res = app.get(getEmbedUrl(url))
+        val resc = res.document.select("script:containsData(eval)").firstOrNull()?.data()
+        resc?.let {
+            val jsonStr2 = AppUtils.parseJson<SvgObject>(runJS2(it))
+            val watchlink = sigDecode(jsonStr2.stream)
+
+            callback.invoke(
+                newExtractorLink(
+                    this.name,
+                    name,
+                    watchlink,
+                ) {
+                    this.referer = mainUrl
+                }
+            )
+        }
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+	private fun sigDecode(url: String): String {
+        val sig = url.split("sig=")[1].split("&")[0]
+        val t = sig.chunked(2)
+            .joinToString("") { (Integer.parseInt(it, 16) xor 2).toChar().toString() }
+            .let {
+                val padding = when (it.length % 4) {
+                    2 -> "=="
+                    3 -> "="
+                    else -> ""
+                }
+                String(Base64.decode((it + padding).toByteArray(Charsets.UTF_8)))
+            }
+            .dropLast(5)
+            .reversed()
+            .toCharArray()
+            .apply {
+                for (i in indices step 2) {
+                    if (i + 1 < size) {
+                        this[i] = this[i + 1].also { this[i + 1] = this[i] }
+                    }
+                }
+            }
+            .concatToString()
+            .dropLast(5)
+        return url.replace(sig, t)
+    }
+
+    private fun runJS2(hideMyHtmlContent: String): String {
+        var result = ""
+        val r = Runnable {
+            val rhino = Context.enter()
+            rhino.initSafeStandardObjects()
+            rhino.setInterpretedMode(true)
+            val scope: Scriptable = rhino.initSafeStandardObjects()
+            scope.put("window", scope, scope)
+            try {
+                rhino.evaluateString(
+                    scope,
+                    hideMyHtmlContent,
+                    "JavaScript",
+                    1,
+                    null
+                )
+                val svgObject = scope.get("svg", scope)
+                result = if (svgObject is NativeObject) {
+                    NativeJSON.stringify(
+                        Context.getCurrentContext(),
+                        scope,
+                        svgObject,
+                        null,
+                        null
+                    ).toString()
+                } else {
+                    Context.toString(svgObject)
+                }
+            } catch (e: Exception) {
+                Log.e("runJS", "Error executing JavaScript: ${e.message}")
+            } finally {
+                Context.exit()
+            }
+        }
+        val t = Thread(ThreadGroup("A"), r, "thread_rhino", 8 * 1024 * 1024) // Increase stack size to 8MB
+        t.start()
+        t.join()
+        t.interrupt()
+        return result
+    }
+	
+    private fun getEmbedUrl(url: String): String {
+        return url.takeIf { it.contains("/d/") || it.contains("/v/") }
+            ?.replace("/d/", "/e/")?.replace("/v/", "/e/") ?: url
+    }
+
+    data class SvgObject(
+        val stream: String,
+        val hash: String
+    )	
+	
 }
 
 class Hglink : StreamWishExtractor() {
