0f90b77d15bbb54481df4979a756b73d0580c8ae
duro92 <pendisu647@gmail.com>
2025-10-10 09:02:40 +0700

    Fix

--- Nomat/src/main/kotlin/com/nomat/NomatPlugin.kt
+++ Nomat/src/main/kotlin/com/nomat/NomatPlugin.kt
@@ -8,5 +8,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class NomatPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Nomat())
+        registerExtractorAPI(Hydrax())
     }
 }
