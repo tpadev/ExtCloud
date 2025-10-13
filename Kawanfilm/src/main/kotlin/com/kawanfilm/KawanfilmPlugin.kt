191843cff45f69c17a08fb975be2cf06cc6ada9b
duro92 <pendisu647@gmail.com>
2025-10-13 14:02:24 +0700

    Fix

---
+++ Kawanfilm/src/main/kotlin/com/kawanfilm/KawanfilmPlugin.kt
@@ -0,0 +1,22 @@
+package com.kawanfilm
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class KawanfilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DutaMovie())
+        registerExtractorAPI(Dingtezuni())
+        registerExtractorAPI(Bingezove())
+        registerExtractorAPI(Mivalyo())
+        registerExtractorAPI(Hglink())
+        registerExtractorAPI(Ryderjet())
+        registerExtractorAPI(Ghbrisk())
+        registerExtractorAPI(Dhcplay())
+        registerExtractorAPI(Gofile())
+        registerExtractorAPI(Movearnpre())
+        registerExtractorAPI(Vidshare())
+    }
+}
