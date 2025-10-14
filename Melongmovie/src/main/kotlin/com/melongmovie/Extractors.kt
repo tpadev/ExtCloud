060546ad632bf8a952c7422b272d22e1aa197f5a
duro92 <pendisu647@gmail.com>
2025-10-14 16:13:09 +0700

    Fix

--- Melongmovie/src/main/kotlin/com/melongmovie/Extractors.kt
+++ Melongmovie/src/main/kotlin/com/melongmovie/Extractors.kt
@@ -12,7 +12,7 @@ import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.extractors.StreamWishExtractor
 import com.lagradost.cloudstream3.extractors.Filesim
 import com.lagradost.cloudstream3.extractors.Evoload
-import com.lagradost.cloudstream3.extractors.Streamlare
+import com.lagradost.cloudstream3.extractors.StreamSB
 import java.net.URI
 
 
@@ -85,7 +85,7 @@ class Veev3 : Evoload() {
     override val mainUrl = "https://veev.to"
 }
 
-class Veev4 : Streamlare() {
+class Veev4 : StreamSB() {
     override val name = "Veev4"
     override val mainUrl = "https://veev.to"
 }
