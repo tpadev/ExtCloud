e486ef77a6de27ccdb3b5c752596e5bfa640133b
duro92 <pendisu647@gmail.com>
2025-11-27 10:22:47 +0700

    Fix

--- Klikxxi/src/main/kotlin/com/klikxxi/Klikxxi.kt
+++ Klikxxi/src/main/kotlin/com/klikxxi/Klikxxi.kt
@@ -121,7 +121,7 @@ class Klikxxi : MainAPI() {
     private fun Element.toRecommendResult(): SearchResponse? {
         val title = this.selectFirst("a > span.idmuvi-rp-title")?.text()?.trim() ?: return null
         val href = this.selectFirst("a")!!.attr("href")
-        val posterUrl = fixUrlNull(this.selectFirst("a > img")?.getImageAttr().fixImageQuality())
+        val posterUrl = fixUrlNull(this.selectFirst("a > img")?.fixPoster().fixImageQuality())
         return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
     }
 
