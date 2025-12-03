7520e8af22003c8145fa0ba2c914bb49d4b94492
Pendztagram <114850487+duro92@users.noreply.github.com>
2025-12-03 12:31:01 +0700

    Update Ngefilm.kt

--- Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
+++ Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
@@ -17,7 +17,7 @@ import org.jsoup.nodes.Element
 
 class Ngefilm : MainAPI() {
 
-    override var mainUrl = "https://new24.ngefilm.site"
+    override var mainUrl = "https://new28.ngefilm.site"
     private var directUrl: String? = null
     override var name = "Ngefilm😎"
     override val hasMainPage = true
