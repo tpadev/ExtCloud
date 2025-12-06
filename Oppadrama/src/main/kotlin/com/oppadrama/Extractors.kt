201ec9e90af35c77fc028e3d966414831a6fe583
Pendztagram <114850487+duro92@users.noreply.github.com>
2025-12-07 03:49:07 +0700

    Update Extractors.kt

--- Oppadrama/src/main/kotlin/com/oppadrama/Extractors.kt
+++ Oppadrama/src/main/kotlin/com/oppadrama/Extractors.kt
@@ -13,6 +13,6 @@ class Smoothpre: VidHidePro() {
 }
 
 class Emturbovid : EmturbovidExtractor() {
-    override var name = "Emturbovid"
-    override var mainUrl = "https://emturbovid.com"
+    override val name = "Emturbovid"
+    override val mainUrl = "https://emturbovid.com"
 }
