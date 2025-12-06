9326ce9c3d80e94cfa7824a5927eb8111ae764a6
ExtCloud <pendtagram@gmail.com>
2025-12-06 21:08:26 +0700

    Fix

--- IdlixProvider/src/main/kotlin/com/hexated/IdlixProviderPlugin.kt
+++ IdlixProvider/src/main/kotlin/com/hexated/IdlixProviderPlugin.kt
@@ -3,11 +3,11 @@ package com.hexated
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.extractors.Jeniusplay
 
 @CloudstreamPlugin
 class IdlixProviderPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(IdlixProvider())
-        registerExtractorAPI(Jeniusplay())
     }
 }
