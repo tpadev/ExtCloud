e5bda62620feabf8014c2982ace6afbaf5df606a
Pendztagram <114850487+duro92@users.noreply.github.com>
2025-12-07 23:25:58 +0700

    Update IdlixProviderPlugin.kt

--- IdlixProvider/src/main/kotlin/com/hexated/IdlixProviderPlugin.kt
+++ IdlixProvider/src/main/kotlin/com/hexated/IdlixProviderPlugin.kt
@@ -3,11 +3,12 @@ package com.hexated
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.extractors.Jeniusplay
+
 
 @CloudstreamPlugin
 class IdlixProviderPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(IdlixProvider())
+registerExtractorAPI(Hglink())  
     }
 }
