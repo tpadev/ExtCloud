b2e2db75cc7df850d021c16b341a24f758cb7a27
duro92 <pendisu647@gmail.com>
2025-12-10 10:17:56 +0700

    Add Anizone provider

---
+++ Anizone/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Anizone.to is a website with no about us section but hey, there are anime."
+    language = "en"
+    authors = listOf("ycngmn")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf("Anime")
+
+
+    // random cc logo i found : me too, how cool
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://anizone.to&size=%size%"
+}
