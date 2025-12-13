bd7a3085acc84202cce3527f20b6783c0f260b33
duro92 <pendisu647@gmail.com>
2025-12-13 10:42:59 +0700

    Add Anichin

---
+++ Anichin/src/main/kotlin/com/anichin/AnichinPlugin.kt
@@ -0,0 +1,13 @@
+package com.anichin
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+
+@CloudstreamPlugin
+class AnichinPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Anichin()))
+    }
+}
