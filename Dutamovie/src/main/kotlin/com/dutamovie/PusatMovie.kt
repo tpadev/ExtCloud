90ce887138e78bda1b16e41447c01c721b52cbf4
ExtCloud <pendtagram@gmail.com>
2025-12-14 20:22:30 +0700

    Fix

--- Dutamovie/src/main/kotlin/com/dutamovie/PusatMovie.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/PusatMovie.kt
@@ -1,5 +1,8 @@
 package com.dutamovie
 
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.httpsify
+import com.lagradost.cloudstream3.utils.loadExtractor
 
 class PusatMovie : DutaMovie() {
     override var mainUrl = "https://carteretliteracy.org"
