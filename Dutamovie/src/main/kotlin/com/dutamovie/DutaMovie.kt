e5a5be570a6acf2132228987837de1254208a42f
Pendztagram <114850487+duro92@users.noreply.github.com>
2025-12-14 18:33:17 +0700

    Update DutaMovie.kt

--- Dutamovie/src/main/kotlin/com/dutamovie/DutaMovie.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutaMovie.kt
@@ -14,7 +14,7 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 import java.net.URI
 import org.jsoup.nodes.Element
 
-class DutaMovie : MainAPI() {
+open class DutaMovie : MainAPI() {
 
     override var mainUrl = "https://pilots-music.com"
     private var directUrl: String? = null
