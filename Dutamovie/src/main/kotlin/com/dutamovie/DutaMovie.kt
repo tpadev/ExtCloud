42031e3d0a85fae1c03e7ffa8b90df6a277ab595
Pendztagram <114850487+duro92@users.noreply.github.com>
2025-12-14 15:40:25 +0700

    Update DutaMovie.kt

--- Dutamovie/src/main/kotlin/com/dutamovie/DutaMovie.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutaMovie.kt
@@ -16,7 +16,7 @@ import org.jsoup.nodes.Element
 
 class DutaMovie : MainAPI() {
 
-    override var mainUrl = "https://goldfingerdesign.com"
+    override var mainUrl = "https://pilots-music.com"
     private var directUrl: String? = null
     override var name = "DutaMovie🎉"
     override val hasMainPage = true
