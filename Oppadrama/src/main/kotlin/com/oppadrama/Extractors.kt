9e98b41a4bfd9df15a04460cea047f125d3fa4e7
duro92 <pendisu647@gmail.com>
2025-12-16 09:22:54 +0700

    Fix

--- Oppadrama/src/main/kotlin/com/oppadrama/Extractors.kt
+++ Oppadrama/src/main/kotlin/com/oppadrama/Extractors.kt
@@ -8,6 +8,7 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.*
 
 
 
