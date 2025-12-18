6773e154234c68ee0872cc8c36758c9eb7829202
duro92 <pendisu647@gmail.com>
2025-12-18 11:07:13 +0700

    Fix Melongmovie

--- Melongmovie/src/main/kotlin/com/melongmovie/Melongmovie.kt
+++ Melongmovie/src/main/kotlin/com/melongmovie/Melongmovie.kt
@@ -27,7 +27,11 @@ class Melongmovie : MainAPI() {
 
     override val mainPage = mainPageOf(
     "$mainUrl/latest-movies/page/%d/" to "Movie Terbaru",
-    )
+    "$mainUrl/advanced-search/page/%d/?order=latest&type%5B%5D=series" to "Series Terbaru"
+    "$mainUrl/advanced-search/page/%d/?order=popular&type%5B%5D=series" to "Series Terpopuler"
+    "$mainUrl/advanced-search/page/%d/?order=popular&type%5B%5D=post" to "Movie Terpopuler"
+)
+
 
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
