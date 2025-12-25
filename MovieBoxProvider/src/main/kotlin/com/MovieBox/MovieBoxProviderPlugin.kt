9a08a6bdaf335ea06c145709ca832e349969596d
ExtCloud <pendtagram@gmail.com>
2025-12-25 14:11:21 +0700

    Fix

--- MovieBoxProvider/src/main/kotlin/com/MovieBox/MovieBoxProviderPlugin.kt
+++ MovieBoxProvider/src/main/kotlin/com/MovieBox/MovieBoxProviderPlugin.kt
@@ -1,14 +1,13 @@
-
-package com.hexated
+package com.MovieBox
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class MovieboxPlugin: Plugin() {
+class MovieBoxProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Moviebox())
+        registerMainAPI(MovieBoxProvider())
     }
 }
