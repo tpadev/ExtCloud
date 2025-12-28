dd8e5fcce8a4eb929773622363e7addbb71f7079
duro92 <pendisu647@gmail.com>
2025-12-28 11:46:06 +0700

    Add Fufafilm

---
+++ Fufafilm/src/main/kotlin/com/fufafilm/FufafilmPlugin.kt
@@ -0,0 +1,26 @@
+package com.ngefilm
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class FufafilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Fufafilm())
+        registerExtractorAPI(Dingtezuni())
+        registerExtractorAPI(Bingezove())
+        registerExtractorAPI(Mivalyo())
+        registerExtractorAPI(Movearnpre())
+        registerExtractorAPI(Dhtpre())
+        registerExtractorAPI(Hglink())
+        registerExtractorAPI(Gdriveplayerto())
+        registerExtractorAPI(Playerngefilm21())
+        registerExtractorAPI(P2pplay())
+        registerExtractorAPI(Xshotcok())
+        registerExtractorAPI(Dsvplay())
+        registerExtractorAPI(Dintezuvio())
+        registerExtractorAPI(Vidhidepre())
+    }
+}
+
