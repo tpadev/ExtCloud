95bd40300119a8d989e6b852533a55901338a71c
duro92 <pendisu647@gmail.com>
2025-12-31 11:50:33 +0700

    Fix

--- Pencurimovie/src/main/kotlin/com/Pencurimovie/PencurimovieProvider.kt
+++ Pencurimovie/src/main/kotlin/com/Pencurimovie/PencurimovieProvider.kt
@@ -2,7 +2,7 @@ package com.Pencurimovie
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-
+import android.content.Context
 
 @CloudstreamPlugin
 class PencurimovieProvider: Plugin() {
