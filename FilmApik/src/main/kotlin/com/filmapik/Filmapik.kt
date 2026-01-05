11611fdda41d18fc8ca7f72e2ba96750bbece9a5
duro92 <pendisu647@gmail.com>
2026-01-05 14:40:01 +0700

    Fix

--- FilmApik/src/main/kotlin/com/filmapik/Filmapik.kt
+++ FilmApik/src/main/kotlin/com/filmapik/Filmapik.kt
@@ -55,7 +55,12 @@ class Filmapik : MainAPI() {
         val a = selectFirst("a[href]") ?: return null
         val img = a.selectFirst("img[src][alt]") ?: return null
         val href = fixUrl(a.attr("href"))
-        val title = img.attr("alt").trim()
+        val rawTitle = img.attr("alt").trim()
+        val title = rawTitle
+        .replace(Regex("(?i)nonton\\s*film"), "")
+        .replace(Regex("\\(\\d{4}\\)"), "")
+        .replace(Regex("(?i)subtitle\\s*indonesia"), "")
+        .trim()
         val poster = fixUrlNull(img.attr("src")).fixImageQuality()
         return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = poster }
     }
